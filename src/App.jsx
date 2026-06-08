eee81c85d11db5709f4185ebd2e602975632c984
zaynabhwayji <zaynabhweiji@gmail.com>
2026-06-08 12:04:52 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ProductCard from './components/ProductCard';
+function App() {
+  return (
+    <div>
+      <h1>ShopReact</h1>
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,11 @@
+function ProductCard() {
+    return (
+        <div className="card">
+            <span className="emoji">■</span>
+            <h3>Classic T-Shirt</h3>
+            <p>$19.99</p>
+            <button>Add to Cart</button>
+        </div>
+    );
+}
+export default ProductCard;
