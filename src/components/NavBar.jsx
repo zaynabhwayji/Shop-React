0a399b60e3542dcee01f9ea2f72845134b249c3f
zaynabhwayji <zaynabhweiji@gmail.com>
2026-07-11 17:49:09 +0300

    add screenshots

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,18 @@
 import { Link } from 'react-router-dom';
 import { useCart } from '../context/CartContext';
+import { useState } from 'react';
 function NavBar({ onCartClick }) {
     const { totalItems } = useCart();
+    const [open, setOpen] = useState(false);
     return (
         <nav className="navbar">
-            <div className="logo">■ ShopReact</div>
-            <ul className="nav-links">
+            <div className="logo" onClick={() => setOpen(!open)}>■ ShopReact</div>
+            <ul className={open ? "nav-links active" : "nav-links"}>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/shop">Shop</Link></li>
                 <li><Link to="/about">About</Link></li>
             </ul>
-            <div className="nav-links">
-            </div>
-            <Link to="/cart"  onClick={() => onCartClick(true)}>
+            <Link to="/cart" onClick={() => onCartClick(true)}>
                 <button className="cart-btn">
                     ■ Cart ({totalItems})
                 </button>
